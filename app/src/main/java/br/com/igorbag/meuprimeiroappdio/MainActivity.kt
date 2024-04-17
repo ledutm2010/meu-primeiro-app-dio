defacbf6ae49dfe4f85ca0284bb258f9974e204c
ledutm2010 <edutm2010@hotmail.com>
2024-04-17 10:31:04 -0300

    Finalizando o desafio: trocando o idioma do texto e a bandeira do pais de acordo com o idioma do aparelho

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -1,14 +1,31 @@
 package br.com.igorbag.meuprimeiroappdio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var logo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        if (Locale.getDefault().getLanguage().equals("pt")) {
+            logo.setImageResource(R.drawable.brasil);
+        }
+        if (Locale.getDefault().getLanguage().equals("en")) {
+            logo.setImageResource(R.drawable.eua);
+        }
+        if (Locale.getDefault().getLanguage().equals("es")) {
+            logo.setImageResource(R.drawable.espanha);
+        }
+        if (Locale.getDefault().getLanguage().equals("ja")) {
+            logo.setImageResource(R.drawable.japao);
+        }
+        if (Locale.getDefault().getLanguage().equals("de")) {
+            logo.setImageResource(R.drawable.alemanha);
+        }
     }
 }
